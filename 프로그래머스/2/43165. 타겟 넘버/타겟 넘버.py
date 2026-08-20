8355805fd79b3102f32f7f882ff5fbf32ceb0dfc
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-08-20 22:53:20 +0900

    [level 2] Title: 타겟 넘버, Time: 130.39 ms, Memory: 11.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def solution(numbers, target):
+
+    def dfs(idx, cur_sum):
+        if idx == len(numbers):
+            return 1 if cur_sum == target else 0
+
+        return dfs(idx+1, cur_sum+numbers[idx]) + dfs(idx+1, cur_sum-numbers[idx])
+
+    return dfs(0, 0)
