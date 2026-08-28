07991af0ec55edec8b70651eec43ff7f70356b7e
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-08-28 23:36:44 +0900

    [level 2] Title: 피로도, Time: 17.69 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -1,17 +1,22 @@
-from itertools import permutations
-
 def solution(k, dungeons):
-    ans = 0
-
-    for p in permutations(dungeons):
-        cur = k
-        cnt = 0
-
-        for min, use in p:
-            if cur >= min:
-                cur -= use
-                cnt += 1
-
-        ans = max(ans, cnt)
-
-    return ans
+    
+    n = len(dungeons)
+    visited = [False]*n
+    max_cnt = 0
+    
+    def dfs(cur_k, cnt):
+        nonlocal max_cnt
+        
+        max_cnt = max(max_cnt, cnt)
+        
+        for i in range(n):
+            min_req, consume = dungeons[i]
+            
+            if not visited[i] and cur_k >= min_req:
+                visited[i] = True
+                dfs(cur_k - consume, cnt+1)
+                visited[i] = False
+                
+    dfs(k, 0)
+    
+    return max_cnt
