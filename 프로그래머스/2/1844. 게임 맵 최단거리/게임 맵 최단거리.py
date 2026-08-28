bd243c50d61378e4d4a38cc863aceeab62c13d3b
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-08-28 23:24:27 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.07 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,34 @@
 from collections import deque
 
 def solution(maps):
+    
     n = len(maps)
     m = len(maps[0])
     visited = [[False]*m for _ in range(n)]
-
-    def bfs(s_x, s_y):
+    
+    def bfs(sx, sy):
         q = deque()
-        q.append((s_x, s_y))
-        visited[s_x][s_y] = True
-        maps[s_x][s_y] = 1
-
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
-
+        q.append((sx, sy))
+        visited[sx][sy] = 0
+        
+        dx = [-1, 0, 1, 0]
+        dy = [0, 1, 0, -1]
+        
         while q:
-            cur_x, cur_y = q.popleft()
-
-            if cur_x == n-1 and cur_y == m-1:
-                return maps[cur_x][cur_y]
-
+            cx, cy = q.popleft()
+            
+            if cx == n-1 and cy == m-1:
+                return maps[n-1][m-1]
+            
             for d in range(4):
-                nx = cur_x+dx[d]
-                ny = cur_y+dy[d]
-
-                if 0 <= nx < n and 0 <= ny < m and not visited[nx][ny]:
-                    if maps[nx][ny] == 1:
+                nx, ny = cx+dx[d], cy+dy[d]
+                
+                if 0 <= nx < n and 0 <= ny < m:
+                    if not visited[nx][ny] and maps[nx][ny] == 1:
                         q.append((nx, ny))
                         visited[nx][ny] = True
-                        maps[nx][ny] = maps[cur_x][cur_y] + 1
-
+                        maps[nx][ny] = maps[cx][cy] + 1
+        
         return -1
-
-    return bfs(0,0)
+                        
+    return bfs(0, 0)
