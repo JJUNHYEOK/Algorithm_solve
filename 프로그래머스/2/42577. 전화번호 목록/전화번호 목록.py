17fe39227efdd6f47ae31af2a72c9cd60c24b701
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-08-30 08:55:34 +0900

    [level 2] Title: 전화번호 목록, Time: 70.04 ms, Memory: 28.4 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,14 +1,9 @@
 def solution(phone_book):
     
-    hashmap = set(phone_book)
+    phone_book.sort()
     
-    for i in range(len(phone_book)):
-        L = len(phone_book[i])
+    for i in range(len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
         
-        for pfx in range(1, L):
-            prefix = phone_book[i][:pfx]
-            
-            if prefix in hashmap:
-                return False
-            
     return True
