e0a1680468d3b09852ab3649d14449e5fcfa9494
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-08-29 17:02:56 +0900

    [level 2] Title: 프로세스, Time: 0.89 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,25 +1,23 @@
 from collections import deque
 
-def solution(priorites, location):
-
+def solution(priorities, location):
+    
     q = deque()
-    n = len(priorites)
-
-    for i in range(n):
-        q.append((i, priorites[i]))
-
+    
+    for i in range(len(priorities)):
+        idx = i
+        val = priorities[i]
+        q.append((val, idx))
+        
     order = 0
-
+    
     while q:
-        cur_idx, cur_val = q.popleft()
-
-        for item in q:
-            if item[1] > cur_val:
-                q.append((cur_idx, cur_val))
-                break
-
+        cur = q.popleft()
+        
+        if q and cur[0] < max(item[0] for item in q):
+            q.append(cur)
+                
         else:
             order += 1
-
-            if cur_idx == location:
+            if cur[1] == location:
                 return order
