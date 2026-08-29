62633e826d7748967125b15e64a1babeb8277cbc
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-08-29 17:39:13 +0900

    [level 1] Title: 모의고사, Time: 1.44 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,18 +1,22 @@
 def solution(answers):
     patterns = [[1,2,3,4,5], [2,1,2,3,2,4,2,5], [3,3,1,1,2,2,4,4,5,5]]
-    scores = [0]*3
+    person = [0]*3
+    n = len(answers)
+    
+    for i in range(n):
+        ans = answers[i]
+        if ans == patterns[0][i%len(patterns[0])]:
+            person[0] += 1
+        if ans == patterns[1][i%len(patterns[1])]:
+            person[1] += 1
+        if ans == patterns[2][i%len(patterns[2])]:
+            person[2] += 1
 
-    for idx, answer in enumerate(answers):
-        for person, pattern in enumerate(patterns):
-            if answer == pattern[idx%len(pattern)]:
-                scores[person] += 1
-
-    max_score = max(scores)
-
+    max_score = max(person)
     result = []
-
-    for i, score in enumerate(scores):
-        if score == max_score:
+    
+    for i in range(len(person)):
+        if person[i] == max_score:
             result.append(i+1)
-
+            
     return result
