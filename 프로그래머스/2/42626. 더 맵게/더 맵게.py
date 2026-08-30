9c7e578bc11b488842d4e5e1b071d8201587b04b
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-08-30 09:05:43 +0900

    [level 2] Title: 더 맵게, Time: 1693.28 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,19 +1,18 @@
 import heapq
 
 def solution(scoville, K):
-    cnt = 0
     heapq.heapify(scoville)
+    cnt = 0
     
     while scoville[0] < K:
         
         if len(scoville) < 2:
             return -1
-    
+        
         first = heapq.heappop(scoville)
         second = heapq.heappop(scoville)
-
-        mixed = first + (second * 2)
-
+        mixed = first + (second*2)
+        
         heapq.heappush(scoville, mixed)
         cnt += 1
     
