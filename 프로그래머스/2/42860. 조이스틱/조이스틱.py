0dc88b5818d70f0edadf60ea0e9d6aa366db9929
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-09-06 00:10:46 +0900

    [level 2] Title: 조이스틱, Time: 0.02 ms, Memory: 11.7 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.py
@@ -1,25 +1,25 @@
 def solution(name):
-    cnt = 0
+
+    cost = 0
     n = len(name)
-    
-    # up-down
-    for char in name:
-        cnt += min(ord(char) - ord('A'), ord('Z') - ord(char)+1)
-        
-    # left-right
-    min_move = n-1
-    
+
+    for char in name: # up - down
+        up = ord(char) - ord('A')
+        down = 26 - up
+        cost += min(up, down)
+
+    move = n - 1
+
     for i in range(n):
-        next_i = i+1
-        
-        while next_i < n and name[next_i] == 'A':
-            next_i += 1
-            
-        move_right_first = 2*i + (n-next_i)
-        move_left_first = 2*(n-next_i) + i
-        
-        min_move = min(min_move, move_right_first, move_left_first)
-        
-    return cnt + min_move
+        nxt_idx = i + 1
+
+        while nxt_idx < n and name[nxt_idx] == 'A':
+            nxt_idx += 1
+
+        move = min(move, 2*i + (n - nxt_idx), i + 2*(n - nxt_idx))
+
+    cost += move
+
+    return cost
     
     
