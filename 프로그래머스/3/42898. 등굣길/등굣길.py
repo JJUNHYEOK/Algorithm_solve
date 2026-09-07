d77ed8d73ce2bb219020eb48f293d2a03e72070e
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-09-07 17:22:12 +0900

    [level 3] Title: 등굣길, Time: 0.82 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -0,0 +1,32 @@
+def solution(m, n, puddles):
+
+    dp = [[0]*m for _ in range(n)]
+    dp[0][0] = 1
+
+    for a, b in puddles:
+        dp[b-1][a-1] = -1
+
+    for i in range(1, n):
+        if dp[i][0] == -1:
+            dp[i][0] = 0
+
+        else:
+            dp[i][0] = dp[i-1][0]
+
+    for j in range(1, m):
+        if dp[0][j] == -1:
+            dp[0][j] = 0
+
+        else:
+            dp[0][j] = dp[0][j-1]
+
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if dp[i][j] == -1:
+                dp[i][j] = 0
+
+            else:
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+    return (dp[n-1][m-1]%(1000000007))
