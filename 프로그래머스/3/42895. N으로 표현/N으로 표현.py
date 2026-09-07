f2809e9a2f067f913035f499bdc0c6d7423be0c8
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-09-07 15:11:31 +0900

    [level 3] Title: N으로 표현, Time: 8.35 ms, Memory: 12.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.py
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.py
@@ -1,28 +1,27 @@
 def solution(N, number):
-    
-    if N == number:
-        return 1
-    
-    # Define the table
+
     dp = [set() for _ in range(9)]
-    
-    # Design the recurrence relation
-    for i in range(1, 9):
+    dp[1] = {int(str(N))}
+
+    if number in dp[1]:
+        return 1
+
+    # 수 이어붙이기
+    for i in range(2, 9):
         dp[i].add(int(str(N)*i))
-        
-        for j in range(1, i):
-            for op1 in dp[j]:
-                for op2 in dp[i-j]:
-                    dp[i].add(op1+op2)
-                    dp[i].add(op1-op2)
-                    dp[i].add(op1*op2)
-                    
-                    if op2 != 0:
-                        dp[i].add(op1//op2)
-                        
+
+        # 사칙연산
+        for k in range(1, i):
+            for a in dp[k]:
+                for b in dp[i-k]:
+                    dp[i].add(a+b)
+                    dp[i].add(a-b)
+                    dp[i].add(a*b)
+
+                    if b != 0:
+                        dp[i].add(a//b)
+
         if number in dp[i]:
             return i
-        
+
     return -1
-    
-    
