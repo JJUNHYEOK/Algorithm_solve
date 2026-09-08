88b973c5da4b0cebccf2abd7e607096507813c2f
Junhyeok Jeong <148296782+JJUNHYEOK@users.noreply.github.com>
2026-09-08 13:50:31 +0900

    [level 4] Title: 도둑질, Time: 324.53 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.py
+++ 프로그래머스/4/42897. 도둑질/도둑질.py
@@ -1,3 +1,24 @@
 def solution(money):
-    answer = 0
-    return answer
+
+    def linear(arr):
+
+        n = len(arr)
+
+        if n == 0:
+            return 0
+
+        dp = [0]*(n+1)
+        dp[1] = arr[0]
+
+        for i in range(2, n+1):
+            dp[i] = max(dp[i-2]+arr[i-1], dp[i-1])
+
+        return dp[n]
+
+    if len(money) == 1:
+        return money[0]
+
+    include_first = linear(money[1:])
+    include_last = linear(money[:-1])
+
+    return max(include_first, include_last)
